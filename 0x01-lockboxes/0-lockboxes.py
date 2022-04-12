bd405192e1c542e82402d930f693277bf5674352
Jtobyy <jtobi8161@gmail.com>
2022-04-12 16:40:10 +0100

    0. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+    """
+    boxes_length = len(boxes)
+    for box in range(1, boxes_length):
+        found = False
+        counter = 0
+        for temp_box in boxes:
+            if counter != box:
+                for key in temp_box:
+                    if key == box:
+                        found = True
+                        break
+                if found == True:
+                    break
+            counter += 1
+        else:
+            return False
+    return True
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,6 @@
+# Lockboxes
+- Project by Carrie Ybay, Software Engineer at Holberton School
+- Allowed editors: vi, vim, emacs
+- The first line of all your files should be exactly #!/usr/bin/python3
+- Your code should use the PEP 8 style (version 1.7.x)
+- All your files must be executable
