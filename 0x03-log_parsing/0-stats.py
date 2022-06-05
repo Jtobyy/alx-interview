85df20fba9e9bd8155f5c054c1a6641564f11096
Jtobyy <jtobi8161@gmail.com>
2022-06-06 00:06:32 +0100

    Testing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -18,7 +18,6 @@ def main():
                          r'\w+/\d.+" (?P<status_code>200|301|'
                          r'400|401|403|404|405|500) '
                          r'(?P<file_size>\d+)\n')
-
     while True:
         try:
             line = sys.stdin.readline()
@@ -32,21 +31,7 @@ def main():
                         if val != 0:
                             print('{}: {}'.format(key, val))
                     continue
-            total_size += int(match.group('file_size'))
-            status_code = match.group('status_code')
-            status_codes_dict[status_code] += 1
-            if i % 10 == 0:
-                print('File size: {}'.format(total_size))
-                for key, val in status_codes_dict.items():
-                    if val != 0:
-                        print('{}: {}'.format(key, val))
-
         except KeyboardInterrupt:
-            print('File size: {}'.format(total_size))
-            for key, val in status_codes_dict.items():
-                if val != 0:
-                    print('{}: {}'.format(key, val))
-            break
-
+            print('okay')
 
 main()
